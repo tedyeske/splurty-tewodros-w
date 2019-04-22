0b85c110a7b30b5244724080bfcba13c82494f6b
tewodros <teddyhailu5437@gmail.com>
2019-04-22 02:01:21 +0000

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   
   root 'quotes#index'
 
+  resources :quotes
+  get 'about', to: 'quotes#about'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
